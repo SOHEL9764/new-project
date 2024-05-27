f5926f93e3ac0ce0960e08522b67277592e40209
sohail.ah91@gmail.com <sohail.ah91@gmail.com>
2024-05-27 16:06:43 +0530

    my commit

--- obj/Debug/net6.0/SampleWebApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/SampleWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af6aa2a07bc2723d6d18cb4e23482e984646a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
